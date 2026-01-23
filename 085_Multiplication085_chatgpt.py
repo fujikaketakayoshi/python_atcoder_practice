53d8dcada224f9206967a91cc9d139f87dfb6ec9
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-01-23 18:21:07 +0900

    素因数分解するのではなく、与えられた数をa=1からa+=1して約分出来るかmod取ってチェックして約分出来る場合は、K1 = K // aとして、 b=aとしてb+=1しながらさらに約分出来るか繰り返す

---
+++ 085_Multiplication085_chatgpt.py
@@ -0,0 +1,19 @@
+K = int(input())
+ans = 0
+
+a = 1
+while a * a * a <= K:
+    if K % a == 0:
+        K1 = K // a
+
+        b = a
+        while b * b <= K1:
+            if K1 % b == 0:
+                c = K1 // b
+                print(K, K1, a, b, c)
+                if b <= c:
+                    ans += 1
+            b += 1
+    a += 1
+
+print(ans)
